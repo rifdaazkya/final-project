43a6bbd98725cd8e4104d2ea41667e7d304f3af7
rifdaazkya <100396878+rifdaazkya@users.noreply.github.com>
2022-06-25 21:04:49 +0700

    Update Seller.js

--- src/Pages/Seller.js
+++ src/Pages/Seller.js
@@ -103,6 +103,7 @@ const Buyer = () => {
                                 adipisicing elit. Exercitationem cupiditate excepturi assumenda ipsum molestias, aut, odit quod
                                 quibusdam quos consequuntur libero incidunt impedit, nam possimus explicabo totam quam qui tempore.
                             </p>
+// rifda //
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                                 et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                                 aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
